9d68f0fa9a6e6117c0aa07e42e01215562c922b6
m.lisnyi <lesagodsfilms@gmail.com>
2024-07-18 22:20:52 +0300

    init

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create()->each(function ($user) {
+            $posts = Post::factory()->count(rand(1, 5))->create([
+                'user_id' => $user->id,
+            ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            $posts->each(function ($post) use ($user) {
+                Comment::factory()->count(rand(0, 2))->create([
+                    'post_id' => $post->id,
+                    'user_id' => $user->id,
+                ]);
+            });
+        });
     }
 }
---
+++ app/resources/js/Pages/Posts/List.vue
@@ -0,0 +1,35 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+const props = defineProps({ posts: Object })
+
+function upFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+</script>
+<template>
+    <Head title="Posts" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Posts</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-3 gap-4">
+                <div v-for="post in props.posts" class="bg-white group my-5 overflow-hidden shadow-lg sm:rounded-lg hover:scale-110 transition-all duration-500 cursor-pointer motion-reduce:transition-none motion-reduce:hover:transform-none">
+                    <div class="relative p-5">
+                        <div class="absolute top-1/2 l;eft-4 text-3xl text-gray-700 group-hover:text-gray-400 transition-all duration-500 cursor-pointer motion-reduce:transition-none motion-reduce:hover:transform-none">
+                            {{ upFirstLetter(post.title) }}
+                        </div>
+                    </div>
+                    <div class="pt-12 pb-4 px-6 text-gray-700">
+                        {{ upFirstLetter(post.body) }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </AuthenticatedLayout>
+</template>
