5103d788af7f7dbeb7120bd81f0c237ea0d54770
Th3Patcher <lesagodsfilms@gmail.com>
2024-08-30 09:02:31 +0300

    Done Users pages

--- app/resources/js/Components/Pagination.vue
+++ app/resources/js/Components/Pagination.vue
@@ -1,25 +1,22 @@
-<script>
+<script setup>
 import { Link } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
-export default {
-    name: "Pagination",
-    components: {
-        Link
-    },
-    props: {
-        pagination: Object,
-    }
-}
+defineProps( {
+    pagination: Object,
+    updatedPageNumber: Function,
+})
 </script>
 
 <template>
     <nav class="relative flex justify-center">
         <template v-for="link in pagination.links" :key="link.label">
             <Link
+                @click.prevent="updatedPageNumber(link)"
                 preserve-scroll
                 :href="link.url ?? ''"
                 v-html="link.label"
-                class="flex items-center justify-center px-3 py-2 my-2 text-sm rounded-lg text-gray-600 "
+                class="flex items-center justify-center px-3 py-1 my-2 text-sm rounded-lg text-gray-600 "
                 :class="{'bg-indigo-200': link.active, 'text-indigo-200 cursor-default': !link.url}"
             />
         </template>
--- app/resources/js/Pages/Users/Users.vue
+++ app/resources/js/Pages/Users/Users.vue
@@ -1,18 +1,50 @@
-<script>
+<script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import {Head} from "@inertiajs/vue3";
+import {Head, router, usePage, Link} from "@inertiajs/vue3";
 import Pagination from "@/Components/Pagination.vue";
+import {ref, computed, watch} from 'vue';
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import api from "@/api.js";
 
-export default {
-    name: "Users",
-    components: {Pagination, Head, AuthenticatedLayout},
-    props: {
-        users: Object,
-    },
-    mounted() {
-        console.log(this.users);
+defineProps( {
+    users: Object,
+    search: String,
+});
+let a;
+const test = api.get('/api/warranty_claims/search')
+    .then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.log(error);
+    });
+console.log(a);
+
+let search = ref(usePage().props.search),
+    pageNumber = ref(1);
+
+let usersUrl = computed(() => {
+    let url = new URL(route("users.list"));
+    url.searchParams.append("page", pageNumber.value);
+
+    if (search.value) {
+        url.searchParams.append("search", search.value);
     }
-}
+
+    return url;
+})
+
+const updatedPageNumber = (link) => {
+    pageNumber.value = link.url.split("=")[1];
+};
+
+watch(() => usersUrl.value,
+    (newUrl) => {
+        router.visit(newUrl, {
+            preserveScroll: true,
+            preserveState: true,
+            replace: true
+        });
+    });
 
 </script>
 
@@ -27,23 +59,32 @@ export default {
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="relative overflow-x-auto rounded-xl shadow-lg">
-                    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-                        <li class="me-2">
-                            <a href="#" aria-current="page" class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Profile</a>
-                        </li>
-                        <li class="me-2">
-                            <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Dashboard</a>
-                        </li>
-                        <li class="me-2">
-                            <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Settings</a>
-                        </li>
-                        <li class="me-2">
-                            <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Contacts</a>
-                        </li>
-                        <li>
-                            <a class="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">Disabled</a>
-                        </li>
-                    </ul>
+                    <div class="flex-row items-center justify-between p-4 space-y-3 sm:flex sm:space-y-0 sm:space-x-4 bg-gray-300">
+                        <div class="w-full md:w-1/2">
+                            <form class="flex items-center">
+                                <label for="simple-search" class="sr-only">Search</label>
+                                <div class="relative w-full">
+                                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <input type="text" id="simple-search" v-model="search" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-indigo-500 focus:border-indigo-500" placeholder="Search" required="">
+                                </div>
+                            </form>
+                        </div>
+                        <Link :href="route('users.create')">
+                            <PrimaryButton
+                                    class="flex items-center justify-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 -ml-1 mb-0.5" viewBox="0 0 20 20" fill="currentColor"
+                                     aria-hidden="true">
+                                    <path
+                                        d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"/>
+                                </svg>
+                                Add new user
+                            </PrimaryButton>
+                        </Link>
+                    </div>
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                         <tr>
@@ -73,7 +114,7 @@ export default {
                         </tr>
                         </tbody>
                     </table>
-                    <Pagination :pagination="users.meta"/>
+                    <Pagination :pagination="users.meta" :updatedPageNumber="updatedPageNumber"/>
                 </div>
             </div>
         </div>
